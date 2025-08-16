c4dda0eca22c68c4ae3f7439a0767b367a0795d9
TrackZone1 <48413426+TrackZone1@users.noreply.github.com>
2025-08-16 21:37:27 +0200

    Refactor photographer page layout and enhance data display functionality

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -5,16 +5,29 @@ async function getPhotographer(id) {
     // et bien retourner le tableau photographers seulement une fois récupéré
     return {
         photographer: data.photographers.find((p) => p.id === parseInt(id)),
+        media: data.media.filter((m) => m.photographerId === parseInt(id)),
     };
 }
 
 async function displayData(photographer) {
     console.log(photographer);
+    const photographerSection = document.querySelector(".photographer_section");
+
+    document.querySelector("#photographerName").textContent = photographer.name;
+    document.querySelector(
+        "#photographerLocation"
+    ).textContent = `${photographer.city}, ${photographer.country}`;
+    document.querySelector("#photographerTagline").textContent =
+        photographer.tagline;
+    const photographerAvatar = document.querySelector("#photographerAvatar");
+    photographerAvatar.innerHTML = `<img src="assets/photographers/${photographer.portrait}" alt="${photographer.name}" />`;
 }
 
 async function init() {
     const params = new URLSearchParams(window.location.search);
     const photographerId = params.get("id");
+
+    console.log(photographerId);
 
     if (!photographerId) {
         console.error("Aucun ID de photographe trouvé dans l'URL");
