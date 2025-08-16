8db6893566273d5850ad69eea01ad4e283204160
TrackZone1 <48413426+TrackZone1@users.noreply.github.com>
2025-08-16 03:49:07 +0200

    Refactor code structure for improved readability and maintainability

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,29 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { id, name, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const article = document.createElement("article");
+        article.setAttribute("data-id", id);
+        const img = document.createElement("img");
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", name);
+        const h2 = document.createElement("h2");
         h2.textContent = name;
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        const p = document.createElement("p");
+        p.textContent = `${city}, ${country}`;
+        article.appendChild(p);
+        const span = document.createElement("span");
+        span.textContent = tagline;
+        article.appendChild(span);
+        const smallPrice = document.createElement("small");
+        smallPrice.textContent = `${price}€/jour`;
+        article.appendChild(smallPrice);
+
+        return article;
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, getUserCardDOM };
 }
